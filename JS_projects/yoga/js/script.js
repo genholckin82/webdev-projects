45af8b2478541337c8066d3cc667dd9436df6b7b
holkin.gen@gmail.com <you@example.com>
2021-07-08 14:33:42 +0500

    project yoga

--- JS_projects/yoga/js/script.js
+++ JS_projects/yoga/js/script.js
@@ -34,4 +34,124 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
   });
+
+ // Timer
+
+ let deadline = '2021-10-21';
+
+ function getTimeRemaining(endtime) {
+   let t  = Date.parse(endtime) - Date.parse(new Date()),
+    seconds = Math.floor((t/1000) % 60),
+     minutes = Math.floor((t/1000/60) % 60),
+    // hours = Math.floor((t/(1000*60*60)));
+    hours = Math.floor((t/1000/60/60) % 24);
+    // days = Math.floor((t/(1000*60*60*24)));
+
+     return {
+       'total': t,
+       'hours': hours,
+       'minutes': minutes,
+       'seconds': seconds
+     };
+    }
+
+ function setClock(id, endtime) {
+   let timer = document.getElementById(id),
+      hours  = timer.querySelector('.hours'),
+      minutes  = timer.querySelector('.minutes'),
+      seconds  = timer.querySelector('.seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+      function updateClock() {
+          let t = getTimeRemaining(endtime);
+          hours.textContent = t.hours;
+          minutes.textContent = t.minutes;
+          seconds.textContent =  t.seconds;
+
+          if (t.total <= 0) {
+            clearInterval(timeInterval);
+             }
+
+         }
+
+     }
+
+ setClock('timer', deadline);
+
+// Modal
+
+let more = document.querySelector('.more'),
+    overlay  = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close');
+
+
+
+
+    more.addEventListener('click', function() {
+      overlay.style.display = 'block';
+      this.classList.add('more-splash');
+      document.body.style.overflow = 'hidden';
+
+    });
+
+    close.addEventListener('click', function() {
+     overlay.style.display = 'none';
+     more.classList.remove('more-splash');
+     document.body.style.overflow = '';
+
+   });
+
+
+    // Form
+   
+    let message = {
+     loading: 'Загрузка...',
+     succes: 'Спасибо!Скоро мы с вами свяжемся!',
+     failure: 'Что-то пошло не так...'
+
+   };
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+        form.addEventListener('submit', function(event) {
+          event.preventDefault();
+          form.appendChild(statusMessage);
+
+          let request = new XMLHttpRequest();
+          request.open('POST', 'server.php');
+         // request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+           request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+          let  formData = new FormData(form);
+
+          let obj = {};
+          formData.forEach(function(value,key) {
+            obj[key] = value;
+             });
+             let json = JSON.stringify(obj);
+
+         // request.send(formData);
+          request.send(json);
+
+          request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+               statusMessage.innerHTML = message.loading;
+            } else if(request.readyState ===4 && request.status == 200) {
+             statusMessage.innerHTML = message.succes;
+            } else {
+             statusMessage.innerHTML = message.failure;
+            }
+
+          });
+
+          for (let i = 0; i < input.length; i++) {
+               input[i].value = '';
+          }
+
+       });
+
+
 });
